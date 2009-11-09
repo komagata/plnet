ec2c28cacbff1991b1f72c130900d4dc8c98efde
Masaki Komagata <komagata@gmail.com>
2009-11-09 22:31:27 +0900

    removed

--- rails/config/initializers/exception_notification.rb
+++
@@ -1,3 +0,0 @@
-ExceptionNotifier.exception_recipients = ["tsuisumi@googlegroups.com"]
-ExceptionNotifier.sender_address = ["Exception Notifier <ossuary-error@noukotsu.net>"]
-ExceptionNotifier.email_prefix = "[ossuary-error] "
--- rails/config/initializers/gettext.rb
+++
@@ -1,7 +0,0 @@
-require 'gettext/rails'
-
-module ActionView
-  class Base
-    delegate :file_exists?, :to => :finder unless respond_to?(:file_exists?)
-  end
-end
